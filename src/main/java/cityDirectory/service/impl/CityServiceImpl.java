62b35d96207fff81f34133030c160d28e78f0cc5
Nobilis <nikolaev-evgeny@yandex.ru>
2022-03-08 09:14:58 +0300

    2022-03-08/12.05 CityDirectory @Transactional

--- src/main/java/cityDirectory/service/impl/CityServiceImpl.java
+++ src/main/java/cityDirectory/service/impl/CityServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CityServiceImpl implements CityService {
 
     private final CityRepository repository;
