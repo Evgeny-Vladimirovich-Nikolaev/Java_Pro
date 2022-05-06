e8e1cfd06e1049554753172e467e92d6566df0d5
Nobilis <nikolaev-evgeny@yandex.ru>
2022-05-06 23:09:24 +0300

    2022-05-06/19.25
    spring-data-rest Реализованы CRUD-операции и переходы

--- spring-data-rest/src/main/java/computerAccessories/repository/AccessoryRepository.java
+++ spring-data-rest/src/main/java/computerAccessories/repository/AccessoryRepository.java
@@ -4,6 +4,6 @@ import computerAccessories.model.Accessory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "computer_accessories")
+@RepositoryRestResource(path = "accessories")
 public interface AccessoryRepository extends JpaRepository<Accessory, String> {
 }
